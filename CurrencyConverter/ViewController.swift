483b2961ac611c6b3fb9bff9fea929fdec7c5b62
Jithesh V K <jitheshvk@192-168-1-107.tpgi.com.au>
2015-10-12 23:56:38 +1100

    Final commit. Code clean and text box current conversion

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -8,22 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController, CustomPickerViewDeledate {
+class ViewController: UIViewController, CustomPickerViewDeledate, UITextFieldDelegate {
 
     //MARK: Properties
 
     @IBOutlet weak var currencyInputTextField: UITextField!
     @IBOutlet weak var currencyOutputLabel: UILabel!
-    
     @IBOutlet weak var currencyPicker: CustomPickerView!
     
     let currencyArray:[String] = ["CAD", "EUR", "GBP", "JPY", "USD"]
     
     let currency = CurrencyValue(baseCurrency: "AUD")
     var toConvertCurrencyType = ""
+    var currentString = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        currencyInputTextField.delegate = self
         
         //Initializing the Custom Picker View with delegate and currecy array
         currencyPicker.assignDelegate(withDelegat: self, currencyFetchList: currencyArray)
@@ -35,13 +37,11 @@ class ViewController: UIViewController, CustomPickerViewDeledate {
         currency.fetchCurrentCurrencyValue(forFetchList: currencyArray)
 
         //To fetch and display rates once the user finishes entering value in the text box
-        currencyInputTextField.addTarget(self, action: "ConvertValue", forControlEvents: .EditingDidEndOnExit)
-        
+        currencyInputTextField.addTarget(self, action: "convertValue", forControlEvents: .EditingDidEndOnExit)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     //MARK: CustomPickerViewDeledate delegate functions
@@ -51,25 +51,56 @@ class ViewController: UIViewController, CustomPickerViewDeledate {
         toConvertCurrencyType = currencyPicker.selectedCurrency
         
         //Recalculate the converted value for selected type
-        ConvertValue()
+        convertValue()
     }
     
     //MARK: Actions
-    func ConvertValue(){
+    func convertValue(){
         
         if (currencyInputTextField.text!.isEmpty){
             print("Empty Text box")
         }
         else {
-            let convertedValueAsString = currency.calculateRate(forType: toConvertCurrencyType, fromValue: Double(currencyInputTextField.text!)!)
-            
+            let convertedValueAsDecimal = currency.calculateRate(forType: toConvertCurrencyType, fromValue: Double(currentString)!)
+
             //To Format the calculated value as per the currency type
-            let _currencyFormatter : NSNumberFormatter = NSNumberFormatter()
-            _currencyFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
-            _currencyFormatter.currencyCode = toConvertCurrencyType
-            let convertValueWithFormat = _currencyFormatter.stringFromNumber(convertedValueAsString)
-            currencyOutputLabel.text = convertValueWithFormat
+            currencyOutputLabel.text = formatCurrency(convertedValueAsDecimal, toType: toConvertCurrencyType)
         }
+        
+    }
+    
+    func formatCurrency(inputDecimal:NSNumber, toType:String)->String{
+        let _currencyFormatter : NSNumberFormatter = NSNumberFormatter()
+        _currencyFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+        _currencyFormatter.currencyCode = toType
+        let convertValueWithFormat = _currencyFormatter.stringFromNumber(inputDecimal)
+        return convertValueWithFormat!
+    }
+    
+    //MARK: TextField Delegate
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool { // return NO to not change text
+
+        switch string {
+        case "0","1","2","3","4","5","6","7","8","9":
+            currentString += string
+            textField.text = formatCurrency(Double(currentString)!, toType: "AUD")
+        
+        case ".":
+            if(!(currentString.rangeOfString(".") != nil)){
+                currentString += string
+            }
+            textField.text = formatCurrency(Double(currentString)!, toType: "AUD")
+            
+        default:
+            var _ = "" //dummy
+        }
+        return false
+    }
+
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        convertValue()
+        textField.resignFirstResponder()
+        return true
     }
 }
 
